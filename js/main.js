351108af0d67bdaf8c2ea19768e8c6873141dbc9
darlandesousa15 <darlandesousa15@gmail.com>
2023-01-17 12:59:03 -0300

    Substuindo em js os data de ids

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 const robotron = document.querySelector("#robotron");
 const somar = document.querySelector("#somar");
 const subtrair = document.querySelector("#subtrair");
-const controle = document.querySelectorAll(".controle-ajuste");
+const controle = document.querySelectorAll("[data-controle]");
 
 controle.forEach( (elemento) => {elemento.addEventListener("click", (evento) => 
-    {manipulaDados(evento.target.textContent, evento.target.parentNode)
+    {manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
 })
 })
 
@@ -19,7 +19,7 @@ dizOi("Darlan")
 
 
 function manipulaDados (operacao, controle) {
-    const peca = controle.querySelector(".controle-contador");
+    const peca = controle.querySelector("[data-contador]");
 
     if (operacao === "-"){
     peca.value = parseInt(peca.value) - 1
