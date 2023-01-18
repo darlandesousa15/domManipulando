1fc236da1d6c0944153d5c2a963b352ad28121fa
darlandesousa15 <darlandesousa15@gmail.com>
2023-01-18 13:04:41 -0300

    Adicionando ao html e js estatisticas

--- js/main.js
+++ js/main.js
@@ -2,11 +2,46 @@ const robotron = document.querySelector("#robotron");
 const somar = document.querySelector("#somar");
 const subtrair = document.querySelector("#subtrair");
 const controle = document.querySelectorAll("[data-controle]");
+const estatisticas = document.querySelectorAll("[data-estatistica]");
 // Este "[data-controle]" é um data-atributes que utilizei para substituir as ids em html
 // evitando com que haja problemas ao substituir nomes de ids
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
 
 controle.forEach( (elemento) => {elemento.addEventListener("click", (evento) => 
     {manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+    atualizaEstatisticas(evento.target.dataset.peca)
 })
 })
 
@@ -29,4 +64,7 @@ function manipulaDados (operacao, controle) {
         peca.value = parseInt(peca.value) + 1
     }
 
+}
+function atualizaEstatisticas (peca) {
+    console.log(pecas[peca])
 }
