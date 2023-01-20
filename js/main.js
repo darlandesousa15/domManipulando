26aaf8d1ed06db0dad18a3e3f490b3c36ee64b11
darlandesousa15 <darlandesousa15@gmail.com>
2023-01-20 06:20:44 -0300

    Trazendo função dizoi novamente

--- js/main.js
+++ js/main.js
@@ -49,13 +49,13 @@ controle.forEach( (elemento) => {elemento.addEventListener("click", (evento) =>
 
 robotron.addEventListener("click", (evento) => {console.log(evento)})
 
-/*function dizOi(nome) {
+function dizOi(nome) {
     console.log('Oi ' + nome)
     console.log('Bem-vindo ao Robotron 2000')
 }
 
 dizOi("Darlan") 
-*/
+
 
 function manipulaDados (operacao, controle) {
     const peca = controle.querySelector("[data-contador]");
